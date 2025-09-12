951c8d339691d3060ea0124cefd3f27212855181
Vladyslav Shutkevych <vladshutkevych@gmail.com>
2025-09-12 08:48:56 +0300

    Training file

---
+++ SwiftCodePlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var greeting = "Hello, playground" //changeable variable
+print(greeting)
+
+greeting = "Hello, world!"
+print(greeting)
+
+var myInt = 5
+var myString = "Hello"
+var myBool = true
+var myFloat: Float = 3.14
+
+print(myInt)
+print(myString)
+print(myBool)
+print(myFloat)
+
+
+
+let myConstantInt = 5 //unchangeable, constant
