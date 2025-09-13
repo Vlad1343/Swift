d569660bbb032b19bf22e957d5fafda7ef4a5652
Vladyslav Shutkevych <vladshutkevych@gmail.com>
2025-09-13 21:35:00 +0300

    Calculator App

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -11,23 +11,52 @@ struct ContentView: View {
     
     let button = [
         ["7", "8", "9", "X"],
-        ["7", "8", "9", "X"],
-        ["1", "2", "3", "X"],
-        ["0", ".", ".", "X"]
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", ".", "="]
     ]
     
+    
+    
     var body: some View {
-        VStack {
-            HStack {
-                ForEach(["7", "8", "9", "X"], id: \.self) { button in Text(button)
-                        .font(.system(size: 32))
-                        .frame(width: 80, height: 80)
-                    
+        
+        ZStack (alignment: .bottom) {
+            Color.black.ignoresSafeArea(edges: .all)
+            
+            VStack (spacing: 12) {
+                HStack {
+                    Spacer()
+                    Text("42")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 64))
+                }.padding()
+
+                
+                ForEach(button, id: \.self) { row in
+                    HStack {
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                            .font(.system(size: 32))
+                            .frame(width: buttonWidth(), height: self.buttonWidth())
+                                .foregroundStyle(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(40)
+
+                        }
+                        
+                    }
                 }
-            }
+            }.padding(.bottom)
         }
     }
+    
+    func buttonWidth() -> CGFloat {
+        80
+    }
+    
+    
 }
+
 
 #Preview {
     ContentView()
