53d05b3050e46f7bc365180cad4c4aed46b44359
Vladyslav Shutkevych <vladshutkevych@gmail.com>
2025-09-13 21:08:37 +0300

    Calculator App

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Vladyslav Shutkevych on 13/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
